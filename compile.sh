a3864f7e4d3df7b4a812deeee4ba92ad295b8193
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2024-08-10 10:36:22 +0200

    chore: compile - remove previous build

--- compile.sh
+++ compile.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
+# Remove previous diesel build
+rm -rf ./dist/diesel.wasm
+rm -rf ./dist/diesel.js
+# Upack diesel.tar.gz
 cd ./source && tar -xzf ./diesel.tar.gz
 cd ../
+# Compile diesel.c to diesel.wasm
 docker run --rm -v ./source:/src -w /src emscripten/emsdk emcc diesel.c \
+    -s WASM=1 \
     -s EXPORTED_FUNCTIONS="['_diesel']" \
-    -s EXPORTED_RUNTIME_METHODS="['allocateUTF8', 'UTF8ToString', 'malloc', 'free']" \
+    -s EXPORTED_RUNTIME_METHODS="['allocateUTF8', 'UTF8ToString']" \
     -o diesel.js
-mv ./source/diesel.js ./dist/diesel.js
+# Move diesel.wasm and diesel.js to dist
 mv ./source/diesel.wasm ./dist/diesel.wasm
+mv ./source/diesel.js ./dist/diesel.js
