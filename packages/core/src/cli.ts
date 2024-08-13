20b972f7e54dfdf999f908a37d974278065c1aab
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2024-08-13 09:54:16 +0200

    feat: init cli

---
+++ packages/core/src/cli.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+import process from "node:process";
+import { parseArgs } from "node:util";
+
+const args = parseArgs({
+	options: {
+		help: {
+			type: "boolean",
+			short: "h",
+		},
+	},
+});
+
+if (process.stdin.isTTY) {
+    if (args.positionals.length === 0) {
+        console.error("Error: No arguments provided.");
+        process.exit(1);
+    }
+	if (
+		args.values.help
+		|| Object.keys(args.values).length === 0
+	) {
+		console.log([
+			"NAME",
+			"	diesel - Dumb Interpretively Executed String Expression Language",
+			"",
+			"SYNOPSIS",
+			"	diesel [EXPRESSION]",
+			"",
+			"DESCRIPTION",
+			"   is the kernel of a macro language you can customise",
+            "   by adding C code and embedding it into your program.",
+			"",
+			"OPTIONS",
+			"	-h, --help",
+			"		Display the help message and exit.",
+			"",
+			"USAGE",
+			"	Evaluate an expression",
+			"",
+			`	diesel $(+,1,2,3,4,5)`,
+			"	output: 15",
+			"",
+		].join("\n"));
+	}
+}
