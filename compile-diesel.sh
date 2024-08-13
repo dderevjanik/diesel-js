8a7f6629f33b8c46ed43d2bca5b82a577fdca911
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2024-08-13 10:14:36 +0200

    chore: commit diesel wasm

--- compile-diesel.sh
+++ compile-diesel.sh
@@ -4,14 +4,14 @@ cd "$(dirname "${0}")" || exit 1
 rm -rf ./dist/diesel.wasm
 rm -rf ./dist/diesel.js
 # Upack diesel.tar.gz
-cd ./src && tar -xzf ./diesel.tar.gz
+cd ./diesel && tar -xzf ./diesel.tar.gz
 cd ../
 # Compile diesel.c to diesel.wasm
-docker run --rm -v ./src:/src -w /src emscripten/emsdk emcc diesel.c \
+docker run --rm -v ./diesel:/src -w /src emscripten/emsdk emcc diesel.c \
     -s WASM=1 \
     -s EXPORTED_FUNCTIONS="['_diesel']" \
     -s EXPORTED_RUNTIME_METHODS="['allocateUTF8', 'UTF8ToString']" \
     -o diesel.js
 # Move diesel.wasm and diesel.js to dist
-mv ./src/diesel.wasm ./dist/diesel.wasm
-mv ./src/diesel.js ./dist/diesel.js
+mv ./diesel/diesel.wasm ./src/diesel.wasm
+mv ./diesel/diesel.js ./src/diesel.js
