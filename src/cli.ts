d088ceabefb8f0db857af7904e7781e620803480
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2024-08-13 10:12:44 +0200

    fix: diesel cli not taking positional args

--- src/cli.ts
+++ src/cli.ts
@@ -2,8 +2,10 @@
 
 import process from "node:process";
 import { parseArgs } from "node:util";
+import { evaluate } from "./index";
 
 const args = parseArgs({
+	allowPositionals: true,
 	options: {
 		help: {
 			type: "boolean",
@@ -12,36 +14,39 @@ const args = parseArgs({
 	},
 });
 
-if (process.stdin.isTTY) {
-    if (args.positionals.length === 0) {
-        console.error("Error: No arguments provided.");
-        process.exit(1);
-    }
-	if (
-		args.values.help
-		|| Object.keys(args.values).length === 0
-	) {
-		console.log([
-			"NAME",
-			"	diesel - Dumb Interpretively Executed String Expression Language",
-			"",
-			"SYNOPSIS",
-			"	diesel [EXPRESSION]",
-			"",
-			"DESCRIPTION",
-			"   is the kernel of a macro language you can customise",
-            "   by adding C code and embedding it into your program.",
-			"",
-			"OPTIONS",
-			"	-h, --help",
-			"		Display the help message and exit.",
-			"",
-			"USAGE",
-			"	Evaluate an expression",
-			"",
-			`	diesel $(+,1,2,3,4,5)`,
-			"	output: 15",
-			"",
-		].join("\n"));
+(async function () {
+	if (process.stdin.isTTY) {
+		if (args.positionals.length > 0) {
+			const evaluated = await evaluate(args.positionals[0]);
+			console.log(evaluated);
+			process.exit(0);
+		}
+		if (
+			args.values.help
+			|| Object.keys(args.values).length === 0
+		) {
+			console.log([
+				"NAME",
+				"	diesel - Dumb Interpretively Executed String Expression Language",
+				"",
+				"SYNOPSIS",
+				"	diesel [EXPRESSION]",
+				"",
+				"DESCRIPTION",
+				"   is the kernel of a macro language you can customise",
+				"   by adding C code and embedding it into your program.",
+				"",
+				"OPTIONS",
+				"	-h, --help",
+				"		Display the help message and exit.",
+				"",
+				"USAGE",
+				"	Evaluate an expression",
+				"",
+				`	diesel $(+,1,2,3,4,5)`,
+				"	output: 15",
+				"",
+			].join("\n"));
+		}
 	}
-}
+})();
