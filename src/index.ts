f0e5b0d86ec6d8db950db1f016d56e785be11989
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2024-08-15 14:50:58 +0200

    feat: add evaluateC func

--- src/index.ts
+++ src/index.ts
@@ -95,6 +95,10 @@ export function parseErrOutput(output: string, position: string) {
 /**
  * DIESEL expression evaluator
  *
+ * This is wrapped C `diesel` function, which will throw custom error message.
+ *
+ * - If you're looking for the original `diesel` function, use `dieselC` function instead.
+ *
  * @param expresion DIESEL expression to evaluate
  * @returns result of the evaluation
  *
@@ -107,12 +111,37 @@ export async function evaluate(expresion: string) {
     const inPtr = Module.allocateUTF8(expresion);
     const outPtr = Module.allocateUTF8(outputStr);
 
-    const position = Module._diesel(inPtr, outPtr);
+    const ret = Module._diesel(inPtr, outPtr);
 	const output = Module.UTF8ToString(outPtr);
-    if (position > 0) { // 0 - means no error
-		const err = parseErrOutput(output, position);
+    if (ret > 0) { // 0 - means no error
+		const err = parseErrOutput(output, ret);
         throw new Error(err.message);
     }
 
     return output;
 }
+
+/**
+ * DIESEL expression evaluator
+ *
+ * This is wrapped C `diesel` function.
+ * Never throws error, instead it returns the error code as original `diesel` function does.
+ *
+ * @param expresion DIESEL expression to evaluate
+ * @returns in `output` you can find the result of the evaluation, and in `return` you can find the error code
+ *
+ * @see {@link https://www.fourmilab.ch/diesel/}
+ */
+export async function evaluateC(expresion: string): Promise<{ output: string; return: number; }> {
+    await moduleInitialized;
+    const outputStr = "";
+
+    const inPtr = Module.allocateUTF8(expresion);
+    const outPtr = Module.allocateUTF8(outputStr);
+
+    const ret = Module._diesel(inPtr, outPtr);
+	const output = Module.UTF8ToString(outPtr);
+
+    return { output, return: ret };
+}
+
