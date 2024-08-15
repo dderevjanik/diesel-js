cb417a731adab705fe9043128bfaff9842e976cf
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2024-08-15 14:18:16 +0200

    test: finish logical, add string functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -251,6 +251,182 @@ describe("Logical", () => {
 
 	});
 
-	// TODO: FINISH
+	describe("$(OR)", () => {
+
+		test("$(or)", async () => {
+			const output = await evaluate("$(or)");
+			assert.equal(output, "0");
+		});
+
+		test("$(or,276)", async () => {
+			const output = await evaluate("$(or,276)");
+			assert.equal(output, "276");
+		});
+
+		test("$(or,0x1000,1)", async () => {
+			const output = await evaluate("$(or,0x1000,1)");
+			assert.equal(output, "4097");
+		});
+
+		test("$(or,0x1000,0x200,0x30,0x4)", async () => {
+			const output = await evaluate("$(or,0x1000,0x200,0x30,0x4)");
+			assert.equal(output, "4660");
+		});
+
+	});
+
+	describe("$(XOR)", () => {
+
+		test("$(xor)", async () => {
+			const output = await evaluate ("$(xor)");
+			assert.equal(output, "0");
+		});
+
+		test("$(xor,276)", async () => {
+			const output = await evaluate ("$(xor,276)");
+			assert.equal(output, "276");
+		});
+
+		test("$(xor,0xF0,0x81)", async () => {
+			const output = await evaluate ("$(xor,0xF0,0x81)");
+			assert.equal(output, "113");
+		});
+
+		test("$(xor,0xF000,0x6200,0x31,0x8005)", async () => {
+			const output = await evaluate ("$(xor,0xF000,0x6200,0x31,0x8005)");
+			assert.equal(output, "4660");
+		});
+
+	});
+
+	describe("$(IF)", () => {
+
+		test("$(if,0,true,false)", async () => {
+			const output = await evaluate("$(if,0,true,false)");
+			assert.equal(output, "false")
+		});
+
+		test("$(if,1,true,false)", async () => {
+			const output = await evaluate("$(if,1,true,false)");
+			assert.equal(output, "true")
+		});
+
+		test("$(if,-2873,true,false)", async () => {
+			const output = await evaluate("$(if,-2873,true,false)");
+			assert.equal(output, "true")
+		});
+
+		test("$(if,1,evaluated,$(bogus bogus not evaluated))", async () => {
+			const output = await evaluate("$(if,1,evaluated,$(bogus bogus not evaluated))");
+			assert.equal(output, "evaluated")
+		});
+
+		test("$(if,0,$(bogus bogus not evaluated),evaluated)", async () => {
+			const output = await evaluate("$(if,0,$(bogus bogus not evaluated),evaluated)");
+			assert.equal(output, "evaluated")
+		});
+
+		test("$(if,1,true only)", async () => {
+			const output = await evaluate("$(if,1,true only)");
+			assert.equal(output, "true only")
+		});
+
+		test("$(if,0,true only)", async () => {
+			const output = await evaluate("$(if,0,true only)");
+			assert.equal(output, "");
+		});
+
+	});
+
+});
+
+describe("String", () => {
+
+	describe("$(STRFILL)", async () => {
+
+		test("$(strfill,,500)", async () => {
+			const output = await evaluate("$(strfill,,500)");
+			assert.equal(output, "");
+		});
+
+		test("$(strfill,.,75)", async () => {
+			const output = await evaluate("$(strfill,.,75)");
+			assert.equal(output, "...........................................................................");
+		});
+
+		test("$(strfill,this never is output,0)", async () => {
+			const output = await evaluate("$(strfill,this never is output,0)");
+			assert.equal(output, "");
+		});
+
+		test("$(strfill,this never is output,-1)", async () => {
+			const output = await evaluate("$(strfill,this never is output,-1)");
+			assert.equal(output, "");
+		});
+
+
+	});
+
+	describe("$(STRLEN)", async () => {
+
+		test("$(strlen,)", async () => {
+			const output = await evaluate("$(strlen,)");
+			assert.equal(output, "0");
+		});
+
+		test("$(strlen, )", async () => {
+			const output = await evaluate("$(strlen, )");
+			assert.equal(output, "1");
+		});
+
+		test("$(strlen,hello there)", async () => {
+			const output = await evaluate("$(strlen,hello there)");
+			assert.equal(output, "11");
+		});
+
+	});
+
+	describe("$(SUBSTR", () => {
+
+		test("$(substr,hello there,7)", async () => {
+			const output = await evaluate("$(substr,hello there,7)");
+			assert.equal(output, "there");
+		});
+
+		test("This is $(substr,hello there,7,3) right answer", async () => {
+			const output = await evaluate("This is $(substr,hello there,7,3) right answer");
+			assert.equal(output, "This is the right answer");
+		});
+
+		test("$(substr,hello there,2,0)", async () => {
+			const output = await evaluate("$(substr,hello there,2,0)");
+			assert.equal(output, "");
+		});
+
+		test("$(substr,hello there,23,12)", async () => {
+			const output = await evaluate("$(substr,hello there,23,12)");
+			assert.equal(output, "");
+		});
+
+	});
+
+	describe("$(UPPER)", () => {
+
+		test("$(upper,lower case)", async () => {
+			const output = await evaluate("$(upper,lower case)");
+			assert.equal(output, "LOWER CASE");
+		});
+
+		test("$(upper,UPPER CASE)", async () => {
+			const output = await evaluate("$(upper,UPPER CASE)");
+			assert.equal(output, "UPPER CASE");
+		});
+
+		test("$(upper,123456789 !@#$%^&* =+\|`~-_ []{} ;: <>./? Mixed Case)", async () => {
+			const output = await evaluate("$(upper,123456789 !@#$%^&* =+\|`~-_ []{} ;: <>./? Mixed Case)");
+			assert.equal(output, "123456789 !@#$%^&* =+\|`~-_ []{} ;: <>./? MIXED CASE");
+		});
+
+	});
 
 });
